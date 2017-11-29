c074e75468509fc9114a398564b7ca905ec081fa
Przemek Gałdys <galdys.przemek@gmail.com>
2017-11-29 21:08:50 +0100

    Zadanie 13.4

--- modules/time.js
+++ modules/time.js
@@ -1,20 +1,20 @@
-var os = require('os');
+var os = require('os')
 
 function getTime() {
-    var uptime = os.uptime();
-    var second = uptime.toFixed(0);
-    var minutes = (uptime / 60).toFixed(0);
-    var hours = (minutes / 60).toFixed(0);
+    var uptime = os.uptime()
+    var second = uptime.toFixed(0)
+    var minutes = (uptime / 60).toFixed(0)
+    var hours = (minutes / 60).toFixed(0)
     if (second < 60) {
-        console.log(second + ' sec');
+        return second + ' sec'
     } else if (second >= 60 && minutes < 60) {
-        second = (uptime % 60).toFixed(0);
-        console.log(minutes + ' min ' + second + ' sec');
+        second = (uptime % 60).toFixed(0)
+        return minutes + ' min ' + second + ' sec'
     } else if (minutes >= 60) {
-        minutes = ((uptime / 60) % 60).toFixed(0);
-        second = (uptime % 60).toFixed(0);
-        console.log(hours + ' h ' + minutes + ' min ' + second + ' sec');
+        minutes = ((uptime / 60) % 60).toFixed(0)
+        second = (uptime % 60).toFixed(0)
+        return hours + ' h ' + minutes + ' min ' + second + ' sec';
     }
-};
+}
 
-exports.give = getTime;
+exports.give = getTime
